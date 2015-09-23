79581bd5c279fbb3352b104f0b71c66a4a48eb01
Marcos Lopez <feirlane@gmail.com>
2015-09-23 10:41:42 +0200

    Improve the Radio superclass so it better serves as an example to implement new Radio modules

--- src/js/radio/radio.js
+++ src/js/radio/radio.js
@@ -1,4 +1,8 @@
 var Radio = function() {
+	var e = new CustomEvent('new_radio', {'detail': {'radio': this, 'name': 'Radio'}});
+	document.dispatchEvent(e);
+
+	this.rds = false;
 };
 
 Radio.prototype.radio_name = 'No radio selected';
@@ -6,7 +10,10 @@ Radio.prototype.stream_url = 'No radio selected';
 Radio.prototype.stream_mimetype = 'No radio selected';
 
 Radio.prototype.get_current_track = function() {
-	var track = {name: 'Track Name', artist: {name: 'Artist Name'}};
-	var e = new CustomEvent('new_rds', {'detail': {track: track}});
-	document.dispatchEvent(e);
+	if (!this.rds) {
+		this.rds = true;
+		var track = {name: 'Track Name', artist: {name: 'Artist Name'}};
+		var e = new CustomEvent('new_rds', {'detail': {track: track}});
+		document.dispatchEvent(e);
+	}
 };
